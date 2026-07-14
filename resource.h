72c568fb8484494ed787b8c91a87c11c38236051
klgplyyiohksiowt <klgplyyiohksiowt@proton.me>
2026-07-14 11:00:43 +0000

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define IDD_MAIN               100
+
+#define IDC_INPUT              1000
+#define IDC_INPUT_BROWSE       1001
+
+#define IDC_OUTPUT             1002
+#define IDC_OUTPUT_BROWSE      1003
+
+#define IDC_KEYS               1004
+#define IDC_KEYS_BROWSE        1005
+
+#define IDC_OVERWRITE          1006
+
+#define IDC_PROGRESS           1007
+#define IDC_LOG                1008
+
+#define IDC_CONVERT            1009
+#define IDC_CANCEL             1010
